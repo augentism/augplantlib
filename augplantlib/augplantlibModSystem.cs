9558d26a0dde74d3bdac0212136d6184431ac7d7
augentism <thecreamcheesemonkey@gmail.com>
2024-05-01 02:59:02 -0400

    implementedBlockCropSisters withpatch

--- augplantlib/augplantlibModSystem.cs
+++ augplantlib/augplantlibModSystem.cs
@@ -7,24 +7,40 @@ using Vintagestory.GameContent;
 using Vintagestory.API.Datastructures;
 using System;
 using Vintagestory.API.MathTools;
+using HarmonyLib;
+using System.Linq;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics;
 
 
 [assembly: ModInfo("augplantlib",
-                    Authors = new string[] { "Unknown" },
-                    Description = "This is a sample mod",
-                    Version = "1.0.0")]
+                    Authors = new string[] { "augentism" },
+                    Description = "Adds Code support for NewWorldCrops",
+                    Version = "0.0.3")]
 
 namespace augplantlib
 {
+    [HarmonyPatch]
     public class augplantlibModSystem : ModSystem
     {
+        public Harmony harmony;
+        public static ICoreAPI api;
         // Called on server and client
         // Useful for registering block/entity classes on both sides
         public override void Start(ICoreAPI api)
         {
             base.Start(api);
+            augplantlibModSystem.api = api;
             //api.Logger.Notification("Hello from template mod: " + api.Side);
             api.RegisterBlockClass("BlockCropFruit", typeof(BlockCropFruit));
+            api.RegisterBlockClass("BlockCropSisters", typeof(BlockCropSisters));
+            var harmony = new Harmony("augplantlib");
+            if(!Harmony.HasAnyPatches(Mod.Info.ModID)) {
+                harmony = new Harmony(Mod.Info.ModID);
+                harmony.PatchAll(); // Applies all harmony patches
+            }
+
         }
 
         public override void StartServerSide(ICoreServerAPI api)
@@ -35,6 +51,47 @@ namespace augplantlib
         public override void StartClientSide(ICoreClientAPI api)
         {
             //api.Logger.Notification("Hello from template mod client side: " + Lang.Get("augplantlib:hello"));
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            harmony?.UnpatchAll(Mod.Info.ModID);
+        }
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(BlockEntityFarmland), "ConsumeNutrients")]
+        public static void consumeRest(BlockEntityFarmland __instance, Block cropBlock)
+        {
+            if(cropBlock is BlockCropSisters)
+            {
+                float nutrientLoss = cropBlock.CropProps.NutrientConsumption / (float)cropBlock.CropProps.GrowthStages;
+                //api.Logger.Event("Farmland is about to consume {0} from {1}!", nutrientLoss, cropBlock.CropProps.RequiredNutrient);
+                //api.Logger.Event("Remaining nutrients:");
+                var nutrientsField = AccessTools.Field(typeof(BlockEntityFarmland), "nutrients");
+                float[] nutrients;
+                
+                    
+                for (int i = 0; i < 2; i++)
+                {
+                    if (nutrientsField != null)
+                    {
+                        nutrients = (float[])nutrientsField.GetValue(__instance);
+                        nutrients[i] = Math.Max(0f, nutrients[i] - nutrientLoss);
+                        //api.Logger.Event("{0}", nutrients[i]);
+
+
+                        MethodInfo methodInfo = AccessTools.Method(typeof(BlockEntityFarmland), "UpdateFarmlandBlock");
+                        if (methodInfo != null)
+                        {
+                            // Invoke the protected method on the instance
+                            methodInfo.Invoke(__instance, null);
+                        }
+                        else
+                        {
+                            api.Logger.Event("Could not find the protected method 'YourProtectedMethod' in BlockEntityFarmland class!");
+                        }
+                    }
+                }
+            }
         }
     }
     public class BlockCropFruit : BlockCrop
@@ -128,5 +185,13 @@ namespace augplantlib
             }
             return world.GetBlock(base.CodeWithParts(nextStage.ToString()));
         }
+    }
+    public class BlockCropSisters : BlockCrop
+    {
+        //does changes nothing about block crop. Is mainly used just for type checking in the harmony patch.
+        //consumeRest is the function inquestion. It is super hacky and pretty terribly implemented with some bad scaling an implications.
+        //In the Json, you must set the nutrient to be consumed to K. This will make sure that it draws from every nutrient evenly.
+        //The way that function works is that it simply subtracts the nutrientLoss from P and N. I also perforn mo sanity checks to see if the nutrients are enough or the scaling.
+        //But adding that would be a lot and im afraid to patch any more than i have to.
     }
 }
